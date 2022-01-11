283d2f063b474f7b454a47be45fa63e77b2fd6a4
JeongwonShin <jennyshin106@gmail.com>
2022-01-11 16:20:46 +0900

    solve: 최대매출 문제 해결 but, sum 구하는 부분을 최적하지 못함

---
+++ section_5/최대매출.js
@@ -0,0 +1,15 @@
+// 시간 복잡도에 있어서 최적화 적용 x => sum을 구하는 부분을 sliding window로 최적화할 수 있을 것 
+function solution(arr, k) {
+  let answer = 0;
+  let sum = 0;
+  for (let i = 0; i < arr.length - k + 1; i++) {
+    let sliced = arr.slice(i, i + k);
+    sum = sliced.reduce((acc, curr) => acc + curr, 0);
+    answer = Math.max(answer, sum);
+  }
+
+  return answer;
+}
+
+let a = [12, 15, 11, 20, 25, 10, 20, 19, 13, 15];
+console.log(solution(a, 3));
